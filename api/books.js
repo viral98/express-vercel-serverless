450ab26d4b249bff17256adbfd013f48218ada55
Viral Tagdiwala <tagdiwalaviral@gmail.com>
2022-11-06 14:05:59 -0800

    Attempt 2

--- api/books.js
+++ api/books.js
@@ -1,6 +1,6 @@
 const app = require("../app");
 const route = require("../routes/api/books");
 app.use("/api/books/", route);
-app.use("/api/books/:id", route);
+app.use(`/api/books/${slug}`, route);
 
 module.exports = app;
